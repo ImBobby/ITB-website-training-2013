3c25b45ec08e2650a4a0c4d3804f85b86fa30fb3
ImBobby <squidz.bee@gmail.com>
2013-11-20 00:49:46 +0700

    add navigation support

--- assets/js/slide.js
+++ assets/js/slide.js
@@ -2,10 +2,32 @@ var App = {
 
     init: function() {
         App.startImpress();
+        App.nav();
     },
 
     startImpress: function() {
         impress().init();
+    },
+
+    nav: function () {
+        var destination,
+            nav     = $('.nav'),
+            trigger = $('.nav__trigger'),
+            menuItem = $('[data-goto]');
+
+        trigger.click(function (e) {
+            e.preventDefault();
+
+            nav.toggleClass('nav--show');
+        });
+
+        menuItem.click(function (e) {
+            e.preventDefault();
+
+            destination = $(this).data('goto');
+            impress().goto(destination);
+            nav.removeClass('nav--show');
+        });
     }
 
 };
