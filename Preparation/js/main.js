fdb30affccd8631835ac0e69f2c544b3e04ed57c
ImBobby <squidz.bee@gmail.com>
2013-11-19 09:08:31 +0700

    add preparation slide

---
+++ Preparation/js/main.js
@@ -0,0 +1,50 @@
+/*******************************************************************************
+
+    Title :  
+    Date  :  January 2013
+    Author:  Suitmedia (http://www.suitmedia.com)
+
+********************************************************************************/
+
+var Site = {
+
+    init: function() {
+        Site.fastClick();
+        Site.enableActiveStateMobile();
+        Site.windowsPhoneViewportFix();
+        Site.impress();
+    },
+
+    fastClick: function() {
+    	FastClick.attach(document.body);
+    },
+
+    enableActiveStateMobile: function(){
+        document.addEventListener('touchstart', function(){}, true);
+    },
+
+    windowsPhoneViewportFix: function() {
+        /** 
+         * Fix windows phone viewport bug
+         * http://timkadlec.com/2013/01/windows-phone-8-and-device-width 
+         */
+         
+        if(navigator.userAgent.match(/IEMobile\/10\.0/)) {
+            var msViewportStyle = document.createElement("style"),
+                fix             = document.createTextNode("@-ms-viewport{width:auto!important}");
+
+            msViewportStyle.appendChild(fix);
+            document.getElementsByTagName('head')[0].appendChild(msViewportStyle);
+        }
+    },
+
+    impress: function() {
+        impress().init();
+    }
+
+};
+
+
+$(function() {
+    Site.init();
+});
